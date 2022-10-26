cb09e50a74547186a1414732d6d55ddfed8e7b7d
itw_yuwb <itw_yuwb@tkmail.com>
2022-10-26 23:10:33 +0800

    🐞 fix(config): 藏起来自己的信息

--- src/config.js
+++ src/config.js
@@ -1,10 +1,4 @@
 // 配置文件，cookie和邮箱配置
 module.exports = {
-  cookie : '_ga=GA1.2.1314505740.1651132980; __tea_cookie_tokens_2608=%7B%22web_id%22%3A%227091562113055278595%22%2C%22user_unique_id%22%3A%227091562113055278595%22%2C%22timestamp%22%3A1651132980427%7D; n_mh=xTk9P9hYhXAY-4POR0bPUK7P8fwaUQDN17ewQbZmKfY; MONITOR_WEB_ID=6de59e09-070b-4e4e-b801-7f1318892113; passport_csrf_token=ab2e788dd9ac0acb0c9cf1b9566bed07; passport_csrf_token_default=ab2e788dd9ac0acb0c9cf1b9566bed07; _tea_utm_cache_2018=undefined; sid_guard=09122190862ce56f505910e3d423f375|1665934624|31536000|Mon,+16-Oct-2023+15:37:04+GMT; uid_tt=adf85aeb7657c28b1bffc52371061d6e; uid_tt_ss=adf85aeb7657c28b1bffc52371061d6e; sid_tt=09122190862ce56f505910e3d423f375; sessionid=09122190862ce56f505910e3d423f375; sessionid_ss=09122190862ce56f505910e3d423f375; sid_ucp_v1=1.0.0-KGE3ZDNiNTAzZjcwNGNmNTk1ZGJjZmY2ODFkYjAwNjRkYzk5YzhlY2MKFwi39JCoyPSrBRCgyrCaBhiwFDgCQO8HGgJsZiIgMDkxMjIxOTA4NjJjZTU2ZjUwNTkxMGUzZDQyM2YzNzU; ssid_ucp_v1=1.0.0-KGE3ZDNiNTAzZjcwNGNmNTk1ZGJjZmY2ODFkYjAwNjRkYzk5YzhlY2MKFwi39JCoyPSrBRCgyrCaBhiwFDgCQO8HGgJsZiIgMDkxMjIxOTA4NjJjZTU2ZjUwNTkxMGUzZDQyM2YzNzU; _tea_utm_cache_2608={"utm_source":"gold_browser_extension"}; _gid=GA1.2.886154581.1666178723',
-  email  : {
-    user: "670739394@qq.com",
-    froms: "670739394@qq.com",
-    to: "670739394@qq.com",
-    pass: "hqblfmszbjiebebh"
-  }
+
 }
